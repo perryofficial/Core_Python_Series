f52c79f1a52be95444a32280e0f5eef4b9b5c6e8
perryofficial <naikprajwal20@gmail.com>
2026-09-07 12:25:17 +0530

    added code in variables

---
+++ basics_python/problem_program_practice/find_square_root.py
@@ -0,0 +1,5 @@
+#using exponentiation operator
+
+num = 16
+num1 = int(input("Enter a number to find its square root: "))
+print("the square root of ", num1, "is", num1 ** 0.5 )
---
+++ practice_js/app.js
@@ -0,0 +1,8 @@
+const findLongestWord = (str) => {
+        if (str.trim().length === 0) {
+            return false;
+        }
+
+  strArr = str.split();
+    console.log(strArr);
+}
